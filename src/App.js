97a7dcb1d91c8984519c166f79a693d6f2590245
jack ning <github@bytedesk.com>
2022-04-20 14:49:56 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Hello World
+        我的crypto工具箱
       </header>
     </div>
   );

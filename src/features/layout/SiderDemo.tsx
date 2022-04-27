382f81b619a55592ba173864d12851aed6ffeec9
jack ning <github@bytedesk.com>
2022-04-27 14:51:58 +0800

    Update SiderDemo.tsx

--- src/features/layout/SiderDemo.tsx
+++ src/features/layout/SiderDemo.tsx
@@ -29,13 +29,13 @@ function getItem(
 
 const items: MenuItem[] = [
   getItem('Option 1', '1', <PieChartOutlined />),
-  getItem('Option 2', '2', <DesktopOutlined />),
+  // getItem('Option 2', '2', <DesktopOutlined />),
   getItem('User', 'sub1', <UserOutlined />, [
     getItem('Tom', '3'),
     getItem('Bill', '4'),
-    getItem('Alex', '5'),
+    // getItem('Alex', '5'),
   ]),
-  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+  // getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
   getItem('Files', '9', <FileOutlined />),
 ];
 
